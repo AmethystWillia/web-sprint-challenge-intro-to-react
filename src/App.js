8efafa0cc8085de22cfda5c076c9413310e04b4d
Amethyst Williams <zmithic@gmail.com>
2021-11-05 11:10:37 -0500

    Imported API; added Character component; mapped over API info; set up Character to display each name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+// Importing cool stuff
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+// Import components
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  // Set state
+  const [swInfo, setswInfo] = useState([]);
+  const [currentSwId, setCurrentSwId] = useState(null);
+
+  const openDetails = name => {
+    setCurrentSwId(name);
+  };
+  const closeDetails = () => {
+    setCurrentSwId(null);
+  }
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  // Calling API
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setswInfo(res.data);
+      })
+      .catch(err => {
+        console.log(`You fucked up! ${err}`);
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {swInfo.map(info => {
+        return <Character info={info} openDetails={openDetails} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+// Importing React
+import React, { useState, useEffect } from 'react';
+
+export default function Character(props) {
+    // Declaring constant
+    const charaInfo = props.info;
+
+    // Return divs
+    return (
+        <div className='character'>
+            <h3>{charaInfo.name}</h3>
+            <button onClick={() => {props.openDetails(charaInfo.name)}}>
+                View Details
+            </button>
+        </div>
+    );
+};
